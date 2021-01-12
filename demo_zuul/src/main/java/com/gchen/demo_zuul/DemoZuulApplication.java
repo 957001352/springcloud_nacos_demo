9ed0f4d4991668045145e17747163ab7da9ea5cd
957001352 <51199996+957001352@users.noreply.github.com>
2021-01-12 17:07:24 +0800

    Update DemoZuulApplication.java

--- demo_zuul/src/main/java/com/gchen/demo_zuul/DemoZuulApplication.java
+++ demo_zuul/src/main/java/com/gchen/demo_zuul/DemoZuulApplication.java
@@ -3,6 +3,7 @@ package com.gchen.demo_zuul;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableZuulProxy
 @SpringBootApplication
 public class DemoZuulApplication {
     public static void main(String[] args) {
